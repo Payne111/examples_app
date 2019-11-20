b0d3f59760ba123bf05896648ffb96c05832050b
panjf <panjf@gegejia.com>
2019-11-20 17:23:09 +0800

    fix: demo展示

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+
+const demoComponents = []
+const examplesContext = require.context('./examples/', true, /src$/)
+examplesContext.keys().forEach(filePath => {
+  const module = examplesContext(filePath)
+  const component = module.default
+  demoComponents.push({
+    key: filePath.split('/')[1],
+    component
+  })
+})
+
+const demoCodeTable = {}
+const demoCodeContext = require.context('./demoCode/', true, /\.js$/)
+demoCodeContext.keys().forEach(filePath => {
+  const module = demoCodeContext(filePath)
+  const code = module.default
+  demoCodeTable[filePath.split('.')[1].replace('/', '')] = code
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {demoComponents.map(compInfo => {
+        const Comp = compInfo.component
+        return (
+          <div key={compInfo.key}>
+            <div>预览</div>
+            <Comp></Comp>
+            <div>代码</div>
+            <pre>{demoCodeTable[compInfo.key]}</pre>
+          </div>
+        )
+      })}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
